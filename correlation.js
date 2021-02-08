db3fdf3341f3b517740abe71420adb81aa0908b1
julesskinner <63356614+julesskinner@users.noreply.github.com>
2021-02-08 20:51:16 +0000

    Arrays saved to vars

--- correlation.js
+++ correlation.js
@@ -667,81 +667,148 @@ function populateFields() {
 
   // Total Amount Spent
   let amountSpent = 0;
+  let amountSpentArr = [];
   $(".cell-1").each(function () {
     var value = $(this).text();
     if (!isNaN(value) && value.length != 0) {
-      amountSpent += parseFloat(value);
+      amountSpentArr.push(value);
+      amountSpent += parseFloat(value, 10);
     }
   });
-  console.log(amountSpent.toFixed(2));
 
   // Total Purchases Conversion Value
-  let Purchases_Conversion_Value = 0;
+  let purchasesConversionValue = 0;
+  let purchasesConversionValueArr = [];
   $(".cell-2").each(function () {
     var value = $(this).text();
     if (!isNaN(value) && value.length != 0) {
-      Purchases_Conversion_Value += parseFloat(value);
+      purchasesConversionValueArr.push(value);
+      purchasesConversionValue += parseFloat(value, 10);
     }
   });
-  console.log(Purchases_Conversion_Value.toFixed(2));
 
   // Total Purchases
   let purchases = 0;
+  let purchasesArr = [];
   $(".cell-3").each(function () {
     var value = $(this).text();
     if (!isNaN(value) && value.length != 0) {
+      purchasesArr.push(value);
       purchases += parseFloat(value);
     }
   });
-  console.log(purchases);
 
   // Total Purchase ROAS (return on ad spend)
-  let Purchase_ROAS = 0;
+  let purchaseROAS = 0;
+  let purchaseROASArr = [];
   $(".cell-4").each(function () {
     var value = $(this).text();
     if (!isNaN(value) && value.length != 0) {
-      Purchase_ROAS += parseFloat(value);
+      purchaseROASArr.push(value);
+      purchaseROAS += parseFloat(value, 10);
     }
   });
-  console.log(Purchase_ROAS.toFixed(2));
 
   // Total Unique checkouts initiated
-  let Unique_checkouts_initiated = 0;
+  let checkoutsInitiated = 0;
+  let checkoutsInitiatedArr = [];
   $(".cell-5").each(function () {
     var value = $(this).text();
     if (!isNaN(value) && value.length != 0) {
-      Unique_checkouts_initiated += parseFloat(value);
+      checkoutsInitiatedArr.push(value);
+      checkoutsInitiated += parseFloat(value);
     }
   });
-  console.log(Unique_checkouts_initiated);
 
   // Total Adds to cart
-  let Adds_to_cart = 0;
+  let addsToCart = 0;
+  let addsToCartArr = [];
   $(".cell-6").each(function () {
     var value = $(this).text();
     if (!isNaN(value) && value.length != 0) {
-      Adds_to_cart += parseFloat(value);
+      addsToCartArr.push(value);
+      addsToCart += parseFloat(value);
     }
   });
-  console.log(Adds_to_cart);
 
   // Total Link clicks
-  let Link_clicks = 0;
+  let linkClicks = 0;
+  let linkClicksArr = [];
   $(".cell-7").each(function () {
     var value = $(this).text();
     if (!isNaN(value) && value.length != 0) {
-      Link_clicks += parseFloat(value);
+      linkClicksArr.push(value);
+      linkClicks += parseInt(value, 10);
     }
   });
-  console.log(Link_clicks);
 
   // Total CTR (link click-through rate)
-  let CTR = 0;
+
+  let ctr = 0;
+  let ctrArr = [];
   $(".cell-8").each(function () {
     var value = $(this).text();
     if (!isNaN(value) && value.length != 0) {
-      CTR += parseFloat(value);
+      ctrArr.push(value);
+      ctr += parseFloat(value, 10);
     }
   });
-  console.log(CTR.toFixed(2));
+
+  cpa = amountSpent / purchases;
+
+  x = purchasesArr;
+  y = linkClicksArr;
+
+  var shortestArrayLength = 0;
+
+  if (x.length == y.length) {
+    shortestArrayLength = x.length;
+  } else if (x.length > y.length) {
+    shortestArrayLength = y.length;
+    console.error(
+      "x has more items in it, the last " +
+        (x.length - shortestArrayLength) +
+        " item(s) will be ignored"
+    );
+  } else {
+    shortestArrayLength = x.length;
+    console.error(
+      "y has more items in it, the last " +
+        (y.length - shortestArrayLength) +
+        " item(s) will be ignored"
+    );
+  }
+
+  var xy = [];
+  var x2 = [];
+  var y2 = [];
+
+  for (var i = 0; i < shortestArrayLength; i++) {
+    xy.push(x[i] * y[i]);
+    x2.push(x[i] * x[i]);
+    y2.push(y[i] * y[i]);
+  }
+
+  var sum_x = 0;
+  var sum_y = 0;
+  var sum_xy = 0;
+  var sum_x2 = 0;
+  var sum_y2 = 0;
+
+  for (var i = 0; i < shortestArrayLength; i++) {
+    sum_x += x[i];
+    sum_y += y[i];
+    sum_xy += xy[i];
+    sum_x2 += x2[i];
+    sum_y2 += y2[i];
+  }
+
+  var step1 = shortestArrayLength * sum_xy - sum_x * sum_y;
+  var step2 = shortestArrayLength * sum_x2 - sum_x * sum_x;
+  var step3 = shortestArrayLength * sum_y2 - sum_y * sum_y;
+  var step4 = Math.sqrt(step2 * step3);
+  var answer = step1 / step4;
+
+  console.log(answer.toFixed(2));
+  document.getElementById("average-order-value").innerHTML = cpa.toFixed(2);
 }
